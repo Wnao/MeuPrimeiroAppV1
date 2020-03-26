9613048bd4f57306b043e62d9afe8312b86ba26e
Wnao <43458392+Wnao@users.noreply.github.com>
2020-03-25 23:57:15 -0300

    ALertDialog

--- app/src/main/java/br/usjt/devmobile/meuprimeiroappv1/MainActivity.java
+++ app/src/main/java/br/usjt/devmobile/meuprimeiroappv1/MainActivity.java
@@ -1,16 +1,24 @@
 package br.usjt.devmobile.meuprimeiroappv1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MEUAPP-MAINACTIVITY";
+
+    Button buttomEnviar;
+    private Button alertDialogButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +64,41 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         if(message.isEmpty()){
             Log.e(TAG,"ERRO-->Usuário tentou clicar no botão sem inserir texto!!");
+
+            alertDialogButton = (Button) findViewById(R.id.buttomEnviar);
+
+            buttomEnviar.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view1){
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Erro!!!");
+                    builder.setMessage("O nome não pode estar vazio.");
+                    builder.setCancelable(false);
+
+                    builder.setPositiveButton("Entendido.", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getApplicationContext(), "OK.",Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.cancel();
+                        }
+                    });
+
+                    AlertDialog alert=builder.create();
+
+                    alert.show();
+
+
+                }
+
+            });
+
         }else {
 
             intent.putExtra("EXTRA_MESSAGE", message);
@@ -65,4 +108,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public void openDialog(){
+
+    }
 }
+
